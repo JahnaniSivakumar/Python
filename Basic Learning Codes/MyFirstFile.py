8a2c1ca1134dd914f75641a49417f6310abcb8ed
JahnaniSivakumar <121501712+JahnaniSivakumar@users.noreply.github.com>
2023-09-16 16:31:42 -0500

    Add files via upload

---
+++ Basic Learning Codes/MyFirstFile.py
@@ -0,0 +1,62 @@
+# Printing the division result of 5 by 8
+print(5 / 8)
+# Printing the sum of 7 and 10
+print(7+10)
+
+#Calculator in Python
+# Addition
+print(4+5)
+# Subtraction
+print(5-5)
+# Multiplication
+print(3*5)
+# Division
+print(10/2)
+
+# Creating a Variable
+savings=100
+# Printing out savings
+print(savings)
+
+#Calculations with Variables
+# Creating the variables monthly_savings and num_months
+monthly_savings = 10
+num_months = 4
+# Multiplying monthly_savings and num_months
+new_savings = monthly_savings * num_months
+# Adding new_savings to your savings
+total_savings = new_savings + savings
+# Printing total_savings
+print(total_savings)
+
+#Understanding other variables
+# Creating a variable half
+half = 0.5
+# Creating a variable intro
+intro = "Hello! How are you?"
+# Creating a variable is_good
+is_good = True
+
+#Operations with other variable types
+monthly_savings = 10
+num_months = 12
+intro = "Hello! How are you?"
+# Calculating year_savings using monthly_savings and num_months
+year_savings = monthly_savings * num_months
+# Printing the type of year_savings
+print(type(year_savings))
+# Assigning sum of intro and intro to doubleintro
+doubleintro = intro + intro
+# Printing  out doubleintro
+print(doubleintro)
+
+#Type Conversions
+# Definition of savings and total_savings
+savings = 100
+total_savings = 150
+# Fixing the printout
+print("I started with $" + str(savings) + " and now have $" + str(total_savings) + ". Awesome!")
+# Definition of pi_string
+pi_string = "3.1415926"
+# Convert pi_string into float: pi_float
+pi_float = float(pi_string)
